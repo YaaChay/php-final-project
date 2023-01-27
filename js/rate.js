8628c1413931f49239a71f44180255f57b77d45d
YaaChay <yachay498@gmail.com>
2023-01-28 02:53:31 +0630

    2:53AM 28/1/2023 | 3cs-36

--- js/rate.js
+++ js/rate.js
@@ -1,5 +1,20 @@
 var rates = document.querySelectorAll(".rate");
+var itemRates = document.querySelectorAll(".itemRate");
 rates.forEach(rate => {
+    for (let i = 1; i <= 5; i++) {
+        var iElement = document.createElement("i");
+        var star = rate.dataset.star;
+        if (i > star) {
+            iElement.setAttribute("class", "bi bi-star");
+            rate.appendChild(iElement);
+        } else {
+            iElement.setAttribute("class", "bi bi-star-fill");
+            rate.appendChild(iElement);
+        }
+
+    }
+});
+itemRates.forEach(rate => {
     for (let i = 1; i <= 5; i++) {
         var iElement = document.createElement("i");
         var star = rate.dataset.star;
